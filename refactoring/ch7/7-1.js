15e950960e91b1cdea40fe458284141d54ed0d30
haesa <xdhaesa@gmail.com>
2023-04-03 12:54:01 +0900

    7.1 레코드 캡슐화하기

--- refactoring/ch7/7-1.js
+++ refactoring/ch7/7-1.js
@@ -1,4 +1,26 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  constructor(data) {
+    this.#data = data;
+    this.#name = data.name;
+    this.#country = data.country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  get rawData() {
+    return { ...this.#data }; // 얕은 복사, cloneDepp
+  }
+}
+
+const organization = new Organization('Acme Gooseberries', 'GB');
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
