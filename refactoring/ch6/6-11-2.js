ac2f2491318905d20601a0cb730797ee21c4f248
haesa <xdhaesa@gmail.com>
2023-04-03 12:25:21 +0900

    6.11.2 단계 쪼개기 챌린지2

--- refactoring/ch6/6-11-2.js
+++ refactoring/ch6/6-11-2.js
@@ -1,18 +1,69 @@
 import fs from 'fs';
 
-if (!process.argv[2]) {
-  throw new Error('파일 이름을 입력하세요');
+// 내가 만든 것
+class Fetch {
+  #args;
+  #fileName;
+  #data;
+  constructor(args) {
+    this.#args = args;
+    this.#fileName = `${this.#args[2]}.json`;
+  }
+
+  validate() {
+    if (!this.#args) {
+      throw new Error('파일 이름을 입력하세요');
+    }
+    if (!fs.existsSync(this.#fileName)) {
+      throw new Error('파일이 존재하지 않습니다');
+    }
+  }
+
+  fetch() {
+    this.validate();
+    this.#data = JSON.parse(fs.readFileSync(this.#fileName));
+  }
+
+  display() {
+    const filtered = this.#args.includes('-r')
+      ? this.#data.filter((order) => order.status === 'ready')
+      : this.#data;
+    console.log(filtered.length);
+  }
 }
 
-const fileName = `./${process.argv[2]}.json`;
-if (!fs.existsSync(fileName)) {
-  throw new Error('파일이 존재하지 않습니다');
+const fetch = new Fetch(process.argv);
+fetch.fetch();
+fetch.display();
+
+// 강의에서 한 것
+run(process.argv);
+
+function run(args) {
+  countOrders(parseCommand(args));
 }
 
-const rawData = fs.readFileSync(fileName);
-const orders = JSON.parse(rawData);
-if (process.argv.includes('-r')) {
-  console.log(orders.filter((order) => order.status === 'ready').length);
-} else {
-  console.log(orders.length);
+function parseCommand(args) {
+  if (!args[2]) {
+    throw new Error('파일 이름을 입력하세요');
+  }
+
+  const fileName = `./${args[2]}.json`;
+  if (!fs.existsSync(fileName)) {
+    throw new Error('파일이 존재하지 않습니다');
+  }
+
+  return {
+    fileName,
+    countReadyOnly: args.includes('-r'),
+  };
+}
+
+function countOrders({ fileName, countReadyOnly }) {
+  const rawData = fs.readFileSync(fileName);
+  const orders = JSON.parse(rawData);
+
+  return countReadyOnly
+    ? orders.filter((order) => order.status === 'ready').length
+    : orders.length;
 }
