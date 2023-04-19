bf7d7d25f39fb3694ff6621b97152fc5ab7a4a19
haesa <xdhaesa@gmail.com>
2023-04-19 23:58:00 +0900

    7.3 기본형을 객체로 바꾸기

--- refactoring/ch7/7-3.js
+++ refactoring/ch7/7-3.js
@@ -2,6 +2,37 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  isHightPriority(orders) {
+    return this.#priority.higherThan('normal');
+  }
+}
+
+class Priority {
+  #value;
+  constructor(value) {
+    if (Priority.legalValues().includes(value)) {
+      this.#value = value;
+    } else {
+      throw new Error(`${value} is invalid for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().findeIndex(this.#value);
+  }
+
+  equals(other) {
+    return this.#index === other.#index;
+  }
+
+  higherThan(other) {
+    return this.#index > other.#index;
+  }
+
+  static legalValues() {
+    return ['low', 'normal', 'high', 'rush'];
+  }
 }
 
 const orders = [
@@ -10,6 +41,4 @@ const orders = [
   new Order({ priority: 'rush' }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHightPriority()).length;
