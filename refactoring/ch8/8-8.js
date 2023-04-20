90384cbb91669aaf449052091d76987d153d9865
haesa <xdhaesa@gmail.com>
2023-04-21 02:30:25 +0900

    8.9 죽은 코드 제거하기

--- refactoring/ch8/8-8.js
+++ refactoring/ch8/8-8.js
@@ -7,12 +7,3 @@ export function acquireData(input) {
     .filter((record) => record[1].trim() === 'India')
     .map((record) => ({ city: record[0].trim(), phone: record[2].trim() }));
 }
-
-const input = `office, country, telephone\n
-Chicago, USA, +1 312 373 1000\n
-Beijing, China, +86 4008 900 505\n
-Bangalore, India, +91 80 4064 9570\n
-Porto Alegre, Brazil, +55 51 3079 3550\n
-Chennai, India, +91 44 660 44766`;
-const result = acquireData(input);
-console.log(result);
