0f6be6b2e7f817d65bb6e230d8dba8c09161d8a7
haesa <xdhaesa@gmail.com>
2023-04-21 01:05:44 +0900

    8.2 필드 옮기기

--- refactoring/ch8/8-2-2.js
+++ refactoring/ch8/8-2-2.js
@@ -1,17 +1,21 @@
 class Account {
   constructor(number, type, interestRate) {
     this._number = number;
-    this._type = type;
-    this._interestRate = interestRate;
+    this._type = new AccountType(type, interestRate);
   }
 
   get interestRate() {
-    return this._interestRate;
+    return this._type.interestRate;
   }
 }
 
 class AccountType {
   constructor(nameString) {
     this._name = nameString;
+    this._interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this._interestRate;
   }
 }
